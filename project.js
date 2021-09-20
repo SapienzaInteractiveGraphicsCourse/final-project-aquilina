9f8b91e6119c24a5e0a8754ac7beb62534b44002
Greta-Aquilina <82372726+Greta-Aquilina@users.noreply.github.com>
2021-09-20 16:16:33 +0200

    Update project.js

--- project.js
+++ project.js
@@ -823,7 +823,7 @@ function init(){
 
     const loaderLightHouseGLTF = new GLTFLoader();
 
-    loaderLightHouseGLTF.load('/lightHouseU/scene.gltf', function(gltfHouse){
+    loaderLightHouseGLTF.load('/final-project-aquilina/lightHouse/scene.gltf', function(gltfHouse){
 
         const objectHouse = gltfHouse.scene;
         objectHouse.castShadow = true;
